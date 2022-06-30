817460fa0e6ed0841ea701a4f5a9772f511a97c5
imshaz <engr.shaz89@gmail.com>
2022-06-30 21:56:10 +0500

    added sub routes and reading params

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
+const usersList = [
+  {
+    id: 1,
+    user: "shax",
+  },
+  {
+    id: 2,
+    user: "imad",
+  },
+  {
+    id: 3,
+    user: "ayesha",
+  },
+];
 function Home() {
   return <h2>Home</h2>;
 }
 function Profile() {
-  return <h2>Profile</h2>;
+  return (
+    <>
+      <h2>Profile</h2>
+      {usersList.map((user) => {
+        return (
+          <div key={user.id}>
+            <Link to={`/profile/${user.id}`}> user : {user.id}</Link>{" "}
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+function UsersProfile(props) {
+  const params = useParams();
+  return (
+    <>
+      <h2>UsersProfile :{params.id}</h2>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <Link to={"/"}>Home</Link> <Link to={"/profile"}>Profile</Link>
+      </nav>
+    </>
+  );
 }
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<UsersProfile />} />
         </Routes>
       </div>
     </BrowserRouter>
