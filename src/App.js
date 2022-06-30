30d913dc5394c3fb0f92cdd2d95192200c33995c
imshaz <engr.shaz89@gmail.com>
2022-06-30 23:05:55 +0500

    added layout with dashboard and auth layout

--- src/App.js
+++ src/App.js
@@ -1,77 +1,92 @@
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
-
-const usersList = [
-  {
-    id: 1,
-    user: "shax",
-  },
-  {
-    id: 2,
-    user: "imad",
-  },
-  {
-    id: 3,
-    user: "ayesha",
-  },
-];
-function Home() {
-  return <h2>Home</h2>;
-}
-function Profile() {
-  return (
-    <>
-      <h2>Profile</h2>
-      {usersList.map((user) => {
-        return (
-          <div key={user.id}>
-            <Link to={`/profile/${user.id}`}> user : {user.id}</Link>{" "}
-          </div>
-        );
-      })}
-    </>
-  );
-}
-
-function UsersProfile(props) {
-  const params = useParams();
-  return (
-    <>
-      <h2>UsersProfile :{params.id}</h2>
-    </>
-  );
-}
-
-function Header() {
-  return (
-    <>
-      <nav>
-        <Link to={"/"}>Home</Link> <Link to={"/profile"}>Profile</Link>
-      </nav>
-    </>
-  );
-}
-
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import "./wireframe.scss";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:id" element={<UsersProfile />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/auth" element={<NonAuthorizedLayout />} />
+        <Route path="/" element={<AuthorizedLayout />}>
+          <Route index element={<DashboardLayout />} />
+
+          <Route path="dashboard" element={<DashboardLayout />}>
+            <Route path="settings" element={<SettingsPage />} />
+            <Route path="search" element={<SearchResultsPage />} />
+          </Route>
+          <Route path="products" element={<ProductsLayout />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
 
+function AuthorizedLayout() {
+  return (
+    <div className="authorized-layout">
+      <div>
+        <header>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/auth">Logout</Link>
+        </header>
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+const DashboardLayout = () => (
+  <div className="dashboard-layout">
+    <aside />
+    <div className="content">
+      <nav>
+        <Link to="/dashboard/settings">Settings</Link>
+        <Link to="/dashboard/search">Search</Link>
+      </nav>
+      <main>
+        {" "}
+        <Outlet />
+      </main>
+    </div>
+  </div>
+);
+
+const ProductsLayout = () => (
+  <div className="products-layout">
+    <main>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </main>
+  </div>
+);
+
+const SettingsPage = () => (
+  <div className="settings-page">
+    <div />
+    <div />
+  </div>
+);
+
+const SearchResultsPage = () => (
+  <div className="search-results-page">
+    <div />
+    <div />
+    <div />
+  </div>
+);
+function NonAuthorizedLayout() {
+  return (
+    <div className="unauthorized-layout">
+      <main>
+        <Link to="/">Login</Link>
+      </main>
+    </div>
+  );
+}
 export default App;
